a95a4832fca8ecfead6254140c73ac6e816b05a0
Jiahaoliuliu <jiahaoliuliu@gmail.com>
2022-02-01 18:14:07 +0400

    First version of ReverseBits

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -2,10 +2,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class ReverseBits {
+
   @EpiTest(testDataFile = "reverse_bits.tsv")
-  public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+  public static long reverseBits(long number) {
+    // Special cases
+    if (number == 0) return 0;
+    int result = 0;
+    int currentPositionRtl = 63;
+
+    while (currentPositionRtl >= 0) {
+      int lastBit = (number & 1) > 0 ? 1 : 0;
+      result += lastBit;
+      result = result << 1;
+      number = number >> 1;
+      currentPositionRtl --;
+      if (number == 0) {
+        result = result << currentPositionRtl;
+        return result;
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
